826959dd420485d58434e1a8f8714255fd9d4af6
rushkock <ruchella_kock@hotmail.com>
2023-08-25 16:31:06 +0200

    move main.m into the src folder and also add requirement.txt

---
+++ src/main.m
@@ -0,0 +1,13 @@
+ ''%% create the folders
+mkdir docs
+mkdir data/raw
+mkdir data/processed
+mkdir src/playground
+mkdir src/utils
+%% create the files
+% you can edit this path
+addpath(genpath('.'))
+edit ./src/main.m
+edit ./README.md
+edit ./requirements.txt
+
