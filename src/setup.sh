2cac3a2c819a4f132838e8411304f50fe5621545
rushkock <ruchella_kock@hotmail.com>
2023-08-25 16:25:17 +0200

    setup.sh file

---
+++ src/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Creating repository structure';
+
+mkdir -p data/raw
+mkdir -p data/processed/figures
+mkdir docs
+mkdir -p src/playground
+mkdir -p src/utils
+touch src/main.m
+touch README.md
+touch requirement.txt
